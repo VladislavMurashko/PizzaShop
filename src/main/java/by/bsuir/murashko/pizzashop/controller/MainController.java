3ee7ce06c8414688071844597f657ef4f744bfaf
Vladislav Murashko <vladislav.murashko@axonim.by>
2020-11-17 21:08:59 +0300

    Add home and about pages

--- src/main/java/by/bsuir/murashko/pizzashop/controller/MainController.java
+++ src/main/java/by/bsuir/murashko/pizzashop/controller/MainController.java
@@ -1,9 +1,20 @@
 package by.bsuir.murashko.pizzashop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "/")
 public class MainController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("home", "Главная страница");
+        return "home";
+    }
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "О нас");
+        return "about";
+    }
 }
