9e0bf4eb5a29a5e54f381d25b8ad6db72f02f599
Vladislav Murashko <vladislav.murashko@axonim.by>
2021-02-17 20:53:43 +0300

    Fix running

--- src/main/java/by/bsuir/murashko/pizzashop/PizzashopApplication.java
+++ src/main/java/by/bsuir/murashko/pizzashop/PizzashopApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PizzashopApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PizzashopApplication.class, args);
     }
-
 }
